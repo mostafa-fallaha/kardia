c7a645e4861dec69bf60827eb9ef79e263bb2f8f
mostafa fallaha <moostafafallaha@gmail.com>
2024-10-05 15:43:41 +0300

    Use RandomOverSampling instead of SMOTE in model training

--- DataScience/model_training.py
+++ DataScience/model_training.py
@@ -1,5 +1,5 @@
 from sklearn.ensemble import RandomForestClassifier
-from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import SMOTE, RandomOverSampler
 from sklearn.model_selection import train_test_split
 
 from feature_selection import feature_selection
@@ -10,8 +10,9 @@ def train_model():
     X = df_model.drop(columns=['had_heart_attack'])
     y = df_model['had_heart_attack']
 
-    smote = SMOTE(random_state=42)
-    X_resampled, y_resampled = smote.fit_resample(X, y)
+    # smote = SMOTE(random_state=42)
+    ros = RandomOverSampler()
+    X_resampled, y_resampled = ros.fit_resample(X, y)
 
     X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.2, stratify=y_resampled, random_state=42)
 
@@ -20,3 +21,4 @@ def train_model():
     rf_clf.fit(X_train, y_train)
 
     return rf_clf, X_test, y_test
+    
